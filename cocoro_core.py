5fe9e686c923d9596c31e7d8e90e08b042a61db3
hirona <hiro.98v7@gmail.com>
2025-05-11 20:16:49 +0900

    LiteLLMに変更

--- cocoro_core.py
+++ cocoro_core.py
@@ -2,7 +2,7 @@ import argparse
 from fastapi import FastAPI
 
 from aiavatar.adapter.http.server import AIAvatarHttpServer
-from aiavatar.sts.llm.chatgpt import ChatGPTService
+from aiavatar.sts.llm.litellm import LiteLLMService
 
 # from aiavatar.sts.llm.gemini import GeminiService
 from config_loader import load_config
@@ -23,44 +23,32 @@ if args.folder_path:
 config = load_config(args.config_dir if hasattr(args, "config_dir") else None)
 
 # setting.jsonから値を取得
-llm_openai_api_key = config.get("characterList", [])[
-    config.get("currentCharacterIndex", 0)
-].get("apiKey")
-llm_gemini_api_key = config.get("characterList", [])[
+llm_api_key = config.get("characterList", [])[
     config.get("currentCharacterIndex", 0)
 ].get("apiKey")
 llm_model = config.get("characterList", [])[config.get("currentCharacterIndex", 0)].get(
     "llmModel"
 )
-# 設定ファイルからポート番号を取得
 port = config.get("cocoroCorePort", 55601)
 
-llm = ChatGPTService(
-    openai_api_key=llm_openai_api_key,
+# https://docs.litellm.ai/docs/providers
+llm = LiteLLMService(
+    api_key=llm_api_key,
     model=llm_model,
-    temperature=0.0,
+    temperature=1.0,
     system_prompt="{system_prompt}",
 )
-
-# llm = GeminiService(
-#     gemini_api_key=LLM_GEMINI_API_KEY,
-#     model=LLM_MODEL,
-#     temperature=0.0,
-#     system_prompt="{system_prompt}",
-# )
 
 # AIAvatarインスタンスを作成
 aiavatar_app = AIAvatarHttpServer(
     llm=llm,
-    system_prompt="{system_prompt}",
-    debug=True,
+    debug=False,
 )
 
 # FastAPIアプリを設定し、AIAvatarのルーターを含める
 app = FastAPI()
 router = aiavatar_app.get_api_router()
 app.include_router(router)
-
 
 # サーバー起動
 if __name__ == "__main__":
